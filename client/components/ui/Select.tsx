d556d89207257a9ce43ed628aabe223136a12dc1
ptsans <kzone416@gmail.com>
2021-03-18 15:36:45 +0800

    feat(client): Add custom Select component

---
+++ client/components/ui/Select.tsx
@@ -0,0 +1,73 @@
+import clsx from 'clsx'
+import styles from 'styles/ui/Select.module.css'
+import {ClassAttributes, HTMLAttributes, useEffect, useRef, useState} from 'react'
+
+type SelectProps = {
+    label: string
+    data: string[]
+    value: number,
+    onChangeOption: (number: number) => void
+}   & ClassAttributes<HTMLDivElement>
+    & HTMLAttributes<HTMLDivElement>
+
+export default function Select(props: SelectProps) {
+    const [isOpen, setOpen] = useState(false)
+    const ref = useRef(null)
+
+    useEffect(() => {
+        return () => {
+            document.body.removeEventListener('click', close)
+
+        }
+    }, [])
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.addEventListener('click', close, {once: true})
+        }
+    }, [isOpen])
+
+    const {
+        className,
+        ...otherProps
+    } = props
+
+    function close(e: MouseEvent) {
+        e && e.target !== ref.current && setOpen(false)
+    }
+
+    return (
+        <div
+            className={clsx(styles.select, className)}
+            {...otherProps}
+        >
+            <div
+                className={styles.select__control}
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                <div
+                    className={styles.select__value}
+                    ref={ref}
+                >
+                    { `${props.label} ${props.data[props.value]}` }
+                </div>
+                <div className={clsx(styles.select__arrow, isOpen && styles.select__arrow_open)} />
+            </div>
+            <div
+                className={clsx(styles.option, isOpen && styles.open__items_open)}
+            >
+                {
+                    props.data.map((option, index)=> (
+                        <button
+                            key={index}
+                            className={clsx(styles.option__item, (index === props.value) && styles.option__item_active)}
+                            onClick={() => props.onChangeOption(index)}
+                        >
+                            {option}
+                        </button>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
