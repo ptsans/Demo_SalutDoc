0fa69ae213ac67aa982006c483c42873dd10e12f
ptsans <kzone416@gmail.com>
2021-03-18 15:51:28 +0800

    feat(client): add dynamic props to title component

--- client/components/ui/Title.tsx
+++ client/components/ui/Title.tsx
@@ -1,8 +1,14 @@
 import styles from 'styles/ui/Title.module.css'
+import {HTMLAttributes} from "react";
+import clsx from "clsx";
 
-export default function Title({ text = '' }) {
+type TitleProps = {
+    text: string
+} & HTMLAttributes<HTMLHeadingElement>
+
+export default function Title({ text = '', className, ...otherProps }: TitleProps) {
     return (
-        <h2 className={styles.title}>
+        <h2 className={clsx(styles.title, className)} {...otherProps}>
             {text}
         </h2>
     )
